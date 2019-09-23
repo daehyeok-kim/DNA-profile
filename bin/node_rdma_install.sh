0f4c87f38ae69e3c8e8d2fe2e2ffb12a4d24c5cc
Daehyeok Kim <dkimcs@gmail.com>
2019-09-23 15:58:26 -0700

    mount additional drive

--- bin/node_rdma_install.sh
+++ bin/node_rdma_install.sh
@@ -43,34 +43,22 @@ EOF
 chmod +x /etc/profile.d/firstboot.sh
 
 export DEBIAN_FRONTEND=noninteractive
-#sed -i -r 's/GRUB_CMDLINE_LINUX_DEFAULT=\"(.*)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 processor.max_cstate=1 intel_idle.max_cstate=0 default_hugepagesz=1GB hugepagesz=1G hugepages=4\"/' /etc/default/grub
-#sed -i -r 's/GRUB_CMDLINE_LINUX_DEFAULT=\"(.*)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 processor.max_cstate=1 intel_idle.max_cstate=0 \"/' /etc/default/grub
-#update-grub
-
-#echo 'auto xenbr0iface xenbr0 inet dhcp\nbridge_ports enp1s0f0\n' >> /etc/network/interfaces
 
 echo "MaxSessions 20" >> /etc/ssh/sshd_config
 
 apt-get update
 apt-get -y install software-properties-common
-add-apt-repository -y ppa:neovim-ppa/stable
 apt-get update
 
 # for building
 apt-get install -y libtool autoconf automake build-essential vim htop tmux libnl-3-dev
 apt-get install -y libffi6 libffi-dev python-dev python-pip
 
-apt-get -y install build-essential
-apt-get -y install bcc bin86 gawk bridge-utils iproute libcurl3 libcurl4-openssl-dev bzip2 module-init-tools transfig tgif
+apt-get -y install build-essential bcc bin86 gawk bridge-utils iproute libcurl3 libcurl4-openssl-dev bzip2 module-init-tools transfig tgif
 apt-get -y install make gcc libc6-dev zlib1g-dev python python-dev python-twisted libncurses5-dev patch libvncserver-dev libsdl-dev libjpeg-dev
 apt-get -y install iasl libbz2-dev e2fslibs-dev git-core uuid-dev ocaml ocaml-findlib libx11-dev bison flex xz-utils libyajl-dev
-apt-get -y install gettext libpixman-1-dev libaio-dev markdown pandoc python-numpy
-apt-get -y install libc6-dev-i386
-apt-get -y install lzma lzma-dev liblzma-dev
-apt-get -y install libsystemd-dev numactl
-apt-get -y install neovim
-apt-get -y install python-dev python-pip python3-dev python3-pip
-
+apt-get -y install gettext libpixman-1-dev libaio-dev markdown pandoc python-numpy libc6-dev-i386 lzma lzma-dev liblzma-dev
+apt-get -y install libsystemd-dev numactl neovim python-dev python-pip python3-dev python3-pip lxc
 apt-get update
 
 # for OFED
@@ -81,8 +69,21 @@ sudo ./mlnx_ofed_linux-4.6-1.0.1.1-ubuntu16.04-x86_64/mlnxofedinstall --all --fo
 echo "options mlx4_core log_num_mgm_entry_size=-1" >> /etc/modprobe.d/mlnx.conf
 /etc/init.d/openibd  restart
 
+# mount additional hard drive to /extra_disk
 mkdir /extra_disk
-/usr/local/etc/emulab/mkextrafs.pl /extra_disk
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sdb
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk
+    # default
+  w # write the partition table
+  q # and we're done
+EOF
+
+mkfs.ext4 /dev/sdb1
+mount /dev/sdb1 /extra_disk
 
 # set the amount of locked memory. will require a reboot
 cat <<EOF  | tee /etc/security/limits.d/90-rmda.conf > /dev/null
