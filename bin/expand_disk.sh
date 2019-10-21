6ccf5fd2035bb428b4a77fd9afcc833d11b2c055
Daehyeok Kim <dkimcs@gmail.com>
2019-10-21 13:22:58 -0700

    test

--- bin/expand_disk.sh
+++ bin/expand_disk.sh
@@ -1,15 +1,2 @@
 #!/bin/sh
 
-# expand /dev/sda1
-swapoff /dev/sda3
-parted /dev/sda 'rm 3 Yes'
-parted /dev/sda print
-parted /dev/sda 'resizepart 1 Yes 100%'
-parted /dev/sda print
-resize2fs /dev/sda1
-# allocate new swap space
-dd if=/dev/zero of=/swapfile bs=1024 count=3145728
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
--- bin/node_rdma_install_14.sh
+++ bin/node_rdma_install_14.sh
@@ -42,6 +42,20 @@ fi
 EOF
 chmod +x /etc/profile.d/firstboot.sh
 
+# expand /dev/sda1
+swapoff /dev/sda3
+parted /dev/sda 'rm 3 Yes'
+parted /dev/sda print
+parted /dev/sda 'resizepart 1 Yes 100%'
+parted /dev/sda print
+resize2fs /dev/sda1
+# allocate new swap space
+dd if=/dev/zero of=/swapfile bs=1024 count=3145728
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+
 export DEBIAN_FRONTEND=noninteractive
 
 echo "MaxSessions 20" >> /etc/ssh/sshd_config
